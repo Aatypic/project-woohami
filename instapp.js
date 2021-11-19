ba9df295a7e88ad82393d06bf25d9f3e84c995a5
TheBeardGuru <al.atahiri@gmail.com>
2021-11-19 17:40:49 +0100

    deuxieme version mobile

---
+++ instapp.js
@@ -0,0 +1,7 @@
+var userFeed = new Instafeed({
+    get: 'user',
+    target: "instafeed",
+    resolution: 'low_resolution',
+    accessToken: 'IGQVJXMUNZATzRodWw3SzZA6eV80V1o1bGoxVXFWZAmJLbFpULW9xWUtyMlFudzI3XzlUVEdUbWNIblNmQmxVcmsyazlGblF5N1V5bXBTdUxKZAVdHUlhzU1BNRUdMbWp4VkpCV0xUblhtTmJkdkF5Tl94WAZDZD'
+});
+userFeed.run();

718521bdc00a60822196d598712a923ccf4cdf92
Aatypic <al.atahiri@gmail.com>
2022-01-19 00:18:38 +0100

    a propos chgmt contenu

--- instapp.js
+++ instapp.js
@@ -2,6 +2,7 @@ var userFeed = new Instafeed({
     get: 'user',
     target: "instafeed",
     resolution: 'low_resolution',
+    // limit: '60',
     accessToken: 'IGQVJYUmVGY1d3M0lVSVlPY0ZATc1NUQ2paaDQ1N284YktnQkpIRzI1ZAHFjU3FSaGpHSzJLMklzYWk2WW5Mb2lna3IwbDFpVG1LcXNjZAGFCWFUtRlh3UmZAGRG44UTh1R1BuSkJNUnBNYUd6YUprdjIxMQZDZD'
 });
 userFeed.run();
